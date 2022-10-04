676e3d761e9f9e0c07d427cf1d07cd07805e45a3
Kevin Ross <kevin.ross@alienfast.com>
2022-10-04 14:43:53 -0500

    Move tsup.config.ts to top level, make it easier to lint, more obvious for the build process.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'tsup'
 
+// this is meant to be executed cwd in the package dir, but shared amongst all via `yarn tsup --config ../../tsup.config.ts`
 export default defineConfig({
   entry: ['src/index.ts'],
   sourcemap: true,
