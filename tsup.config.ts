ecf8511d1bdd1d8dbf37d7218c8e761ade30cd13
Kevin Ross <kevin.ross@alienfast.com>
2022-10-25 14:38:25 -0500

    stop minimizing build, unnecessary.

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   clean: true,
   // dts: true, // bug with project references https://github.com/egoist/tsup/issues/647#issuecomment-1268777707
   format: ['esm'],
-  minify: true,
+  // minify: true,
 })
