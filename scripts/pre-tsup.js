fe3292a9c014ee38628f49f2c0a0236f291da139
Kevin Ross <kevin.ross@alienfast.com>
2023-04-29 14:50:37 -0500

    This went from right, to wrong, to wildly incorrect, back to right

--- scripts/pre-tsup.js
+++ scripts/pre-tsup.js
@@ -1,6 +1,6 @@
 import { rimraf } from 'rimraf'
 
-await rimraf('./packages/*/dist/**/*.{d.ts,d.ts.map}')
+await rimraf('./packages/*/dist/**/*.{js,js.map}', { glob: true })
 
 // remove incremental build metadata - as it is no longer valid.
-await rimraf('./**/tsconfig.tsbuildinfo')
+await rimraf('./**/tsconfig.tsbuildinfo', { glob: true })
