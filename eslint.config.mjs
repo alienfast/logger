b173d7a23192aac4fcb9a1210813858311a3b7ac
Kevin Ross <kevin.ross@alienfast.com>
2024-07-04 13:40:46 -0500

    update dependencies, eslint 9x is working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,26 @@
+import af from '@alienfast/eslint-config'
 import tseslint from 'typescript-eslint'
 
-import af from '@alienfast/eslint-config'
+/**
+ * Project eslint configuration.
+ *
+ * View config with `npx @eslint/config-inspector`
+ */
+export default tseslint.config({
+  name: 'project',
+  extends: [...af.configs.recommended],
+  // languageOptions: {
+  //   parserOptions: {
+  //     // project: true, // find the closest tsconfig file. ['./tsconfig*.json', './packages/*/tsconfig.json'],
+  //     // project: [
+  //     //   //
+  //     //   './tsconfig*.json',
+  //     //   './packages/*/tsconfig.json',
+  //     // ],
 
-// npx @eslint/config-inspector
-export default tseslint.config(
-  {
-    name: 'project',
-    extends: [...af.configs.recommended],
-  },
-  // {
-  //   ignores: ['eslint.config.mjs'],
+  //     warnOnUnsupportedTypeScriptVersion: true,
+  //     EXPERIMENTAL_useProjectService: true,
+  //     // tsconfigRootDir: import.meta.dirname,
+  //   },
   // },
-)
+})
--- packages/logger-browser/src/BrowserLogWriter.ts
+++ packages/logger-browser/src/BrowserLogWriter.ts
@@ -1,7 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable no-console */
 import { browser, isNode, jsonify, Level, LogWriter } from '@alienfast/logger'
 
