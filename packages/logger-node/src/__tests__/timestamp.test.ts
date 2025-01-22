663d9f5e57e6ba1ec11491364d2c2653d4c25738
Kevin Ross <kevin.ross@alienfast.com>
2025-01-22 15:55:10 -0600

    timstamp only test utc values due to CI difference in time zone settings

--- packages/logger-node/src/__tests__/timestamp.test.ts
+++ packages/logger-node/src/__tests__/timestamp.test.ts
@@ -6,10 +6,11 @@ describe('timestamp', () => {
   // const date = new Date('2025-01-02T03:04:05.006Z')
   const date = new Date(Date.UTC(2025, 0, 2, 3, 4, 5, 6))
   it('should format date', () => {
-    // not sure why CI is giving me trouble - perhaps date settings on container? skip for now
+    // CI gives me grief on these values when using non-utc, so just test utc
+
     // expect(timestamp('YYYY-MM-DD HH:mm:ss.ms', false, date)).toEqual('2025-01-01 21:04:05.006')
-    // expect(timestamp('YYYY-MM-DD HH:mm:ss.ms', true, date)).toEqual('2025-01-02 03:04:05.006')
-    expect(timestamp('HH:mm:ss', false, date)).toEqual('21:04:05')
-    // expect(timestamp('HH:mm:ss', true, date)).toEqual('03:04:05')
+    expect(timestamp('YYYY-MM-DD HH:mm:ss.ms', true, date)).toEqual('2025-01-02 03:04:05.006')
+    // expect(timestamp('HH:mm:ss', false, date)).toEqual('21:04:05')
+    expect(timestamp('HH:mm:ss', true, date)).toEqual('03:04:05')
   })
 })
