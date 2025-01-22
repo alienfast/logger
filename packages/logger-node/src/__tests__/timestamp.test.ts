37cccefc8f2d1613a00de1ca7ec33aebe542665b
Kevin Ross <kevin.ross@alienfast.com>
2025-01-22 15:34:45 -0600

    switch test toEqual

--- packages/logger-node/src/__tests__/timestamp.test.ts
+++ packages/logger-node/src/__tests__/timestamp.test.ts
@@ -5,9 +5,9 @@ import { timestamp } from '../timestamp.js'
 describe('timestamp', () => {
   const date = new Date('2025-01-02T03:04:05.006Z')
   it('should format date', () => {
-    expect(timestamp('YYYY-MM-DD HH:mm:ss.ms', false, date)).toBe('2025-01-01 21:04:05.006')
-    expect(timestamp('YYYY-MM-DD HH:mm:ss.ms', true, date)).toBe('2025-01-02 03:04:05.006')
-    expect(timestamp('HH:mm:ss', false, date)).toBe('21:04:05')
-    expect(timestamp('HH:mm:ss', true, date)).toBe('03:04:05')
+    expect(timestamp('YYYY-MM-DD HH:mm:ss.ms', false, date)).toEqual('2025-01-01 21:04:05.006')
+    expect(timestamp('YYYY-MM-DD HH:mm:ss.ms', true, date)).toEqual('2025-01-02 03:04:05.006')
+    expect(timestamp('HH:mm:ss', false, date)).toEqual('21:04:05')
+    expect(timestamp('HH:mm:ss', true, date)).toEqual('03:04:05')
   })
 })
