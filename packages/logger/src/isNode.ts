4c6e91de8f047a67304af2059b721b1a8426f84b
Kevin Ross <kevin.ross@alienfast.com>
2025-10-02 08:38:59 -0500

    revert biome-changed check for isNode

--- packages/logger/src/isNode.ts
+++ packages/logger/src/isNode.ts
@@ -1,1 +1,2 @@
-export const isNode = process?.versions && !!process.versions.node
+/** biome-ignore-all lint/complexity/useOptionalChain: this needs to carefully run in both node and browser environments */
+export const isNode = typeof process !== 'undefined' && process.versions && !!process.versions.node
