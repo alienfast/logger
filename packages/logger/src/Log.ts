c7c4b944d2182a5cfa27fc8efb89e052f702172e
Kevin Ross <kevin.ross@alienfast.com>
2025-10-02 08:53:30 -0500

    handle process carefully for browser compatibility

--- packages/logger/src/Log.ts
+++ packages/logger/src/Log.ts
@@ -12,7 +12,8 @@ export interface Options {
   systemThreshold: Level
 }
 
-const NOLOG = process?.env.NOLOG
+// biome-ignore lint/complexity/useOptionalChain: need careful node/browser handling
+const NOLOG = typeof process !== 'undefined' && process.env.NOLOG
 
 // class SelfBuildLogWriter implements LogWriter {
 //   public write(name: string, level: Level, ...args: any[]) {
@@ -103,13 +104,6 @@ export class Log {
 
   private writeLog(level: Level, args: any[]) {
     if (!globalThis.logWriter) {
-      // if in fact we are building our own logger repo, we need to force this setting because elsewhere we rely on peer dependency
-      // if (process && process.env && process.env.FORCE_LOG_WRITER === 'node') {
-      //   console.warn('Forcing globalThis.logWriter to SelfBuildLogWriter')
-      //   globalThis.logWriter = new SelfBuildLogWriter()
-      //   return
-      // }
-
       dumpConfiguration()
       throw new Error(
         'globalThis.logWriter was not set prior to attempt to write log.  Please use @alienfast/logger-browser or @alienfast/logger-node to initialize a writer at the entry point.',
